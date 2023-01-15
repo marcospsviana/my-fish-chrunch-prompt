daa549e6252d8a0a7e98f40fa4a22f2b369a0b88
marcospsviana <marcospaulo.silvaviana@gmail.com>
2023-01-15 11:39:47 -0300

    alterado arquivo install e readme

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ if [ "$FISH" = "/usr/bin/fish" ]; then
             mkdir "$DIRECTORY"
             cp fish_prompt.fish "$DIRECTORY"
         fi
-        echo "Fish prompt is configured successfull!!!"
+        echo "style prompt is configured successfull!!!"
     )
 else
-    echo "fish is not installed, sorry, but to proceeed with scrpit fish is needed!!"
+    echo "the shell fish is not installed, sorry, but to proceeed with scrpit fish is needed!!"
     
 fi
