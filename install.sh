4f078b501e3faca3be0129097a2b3d683c7932ce
marcospsviana <marcospaulo.silvaviana@gmail.com>
2023-01-15 10:59:43 -0300

    refatorado readme

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ if [ "$FISH" = "/usr/bin/fish" ]; then
                 (
                     echo "Found $FILE_PROMPT moving file to $FILE_PROMPT_BKP"
                     mv "$FILE_PROMPT" "$FILE_PROMPT_BKP"
+                    wget https://github.com/marcospsviana/my-fish-prompt/blob/main/fish_prompt.fish
                     cp fish_prompt.fish "$DIRECTORY"
+                    rm fish_prompt.fish
                 )
             else
                 cp fish_prompt.fish "$DIRECTORY"
