2016962992eff577b703eec07eeca3e3f42e66d2
marcospsviana <marcospaulo.silvaviana@gmail.com>
2023-02-25 06:28:39 -0300

    change color blue to 228FAF

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,5 +3,5 @@ function fish_prompt
     set -g __fish_git_prompt_show_informative_status auto
 
     echo -s (set_color white)"{" (set_color FFBB00) (date "+%H:%M") \
-    (set_color white)"}" (set_color blue) (prompt_pwd) (set_color FF9900)(fish_git_prompt) (set_color white) $prompt_symbol (set_color normal)
+    (set_color white)"}" (set_color 228FAF) (prompt_pwd) (set_color FF9900)(fish_git_prompt) (set_color white) $prompt_symbol (set_color normal)
 end
