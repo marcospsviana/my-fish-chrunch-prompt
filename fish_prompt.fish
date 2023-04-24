0b1cc6da7e371c5c3774f0280cc849573dd856d9
marcospsviana <marcospaulo.silvaviana@gmail.com>
2023-04-24 10:41:23 -0300

    adicionado espaços prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,6 +2,6 @@ function fish_prompt
     set -l prompt_symbol ' ➭ '
     set -g __fish_git_prompt_show_informative_status auto
 
-    echo -s (set_color white)"{" (set_color FFCC99) (date "+%H:%M") \
-    (set_color white)"}" (set_color 228FAF) (prompt_pwd) (set_color FFCC66)(fish_git_prompt) (set_color white) $prompt_symbol (set_color normal)
+    echo -s (set_color white)"{ " (set_color FFCC99) (date "+%H:%M") \
+    (set_color white)" } " (set_color 228FAF) (prompt_pwd) (set_color FFCC66)(fish_git_prompt) (set_color white) $prompt_symbol (set_color normal)
 end
