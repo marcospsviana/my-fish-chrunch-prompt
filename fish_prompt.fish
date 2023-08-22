f3250651204f32a41cea4048fdbf7f4e41ba98d2
marcospsviana <marcospaulo.silvaviana@gmail.com>
2023-08-22 20:17:45 -0300

    change color prompt pwd for 009966

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,5 +3,5 @@ function fish_prompt
     set -g __fish_git_prompt_show_informative_status auto
 
     echo -s (set_color white)"{"(set_color F79E56)(date "+%H:%M")\
-    (set_color white)"}" (set_color blue) (prompt_pwd) (set_color FF9900)(fish_git_prompt) (set_color white) $prompt_symbol (set_color normal)
+    (set_color white)"}" (set_color 009966) (prompt_pwd) (set_color FF9900)(fish_git_prompt) (set_color white) $prompt_symbol (set_color normal)
 end
